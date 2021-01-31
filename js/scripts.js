9d71eb3c4498293e00c92cfb1df42b2fd32b1893
Carlos Urquiza <webquiza@gmail.com>
2021-01-30 18:24:16 -0800

    add final touches to logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  // $("#answers").hide();
+
   $("form#survey").submit(function(event) {
     event.preventDefault();
     const hours = $("#hours").val();
@@ -6,12 +8,84 @@ $(document).ready(function() {
     const experience = $("#experience").val();
     const reason = $("#reason").val();
     const pets = $("#pets").val();
-  
-    $("#output1").text(hours);
-    $("#output2").text(months);
-    $("#output3").text(experience);
-    $("#output4").text(reason);
-    $("#output5").text(pets);
+
+    // declaring my score
+
+    let javascriptScore = 0;
+    let pythonScore = 0;
+    let sharpScore = 0;
+    
+    // Hours Branch
+
+    if (hours === "javascript") {
+      javascriptScore += 1;
+    } else if (hours === "python") {
+      pythonScore += 1;
+    } else if (hours === "sharp") {
+      sharpScore += 1;
+    } else {
+      alert("You didn't answer you fuck!");
+    }
+
+    // Months Branch
+
+    if (months === "javascript") {
+      javascriptScore += 1;
+    } else if (months === "python") {
+      pythonScore += 1;
+    } else if (months === "sharp") {
+      sharpScore += 1;
+    } else {
+      alert("You didn't answer you fuck!");
+    }
+
+    // Experience Branch
+
+    if (experience === "javascript") {
+      javascriptScore += 1;
+    } else if (experience === "python") {
+      pythonScore += 1;
+    } else if (experience === "sharp") {
+      sharpScore += 1;
+    } else {
+      alert("You didn't answer you fuck!");
+    }
+
+    // Reason Branch
+
+    if (reason === "javascript") {
+      javascriptScore += 1;
+    } else if (reason === "python") {
+      pythonScore += 1;
+    } else if (reason === "sharp") {
+      sharpScore += 1;
+    } else {
+      alert("You didn't answer you fuck!");
+    }
+
+    // Pets Branch
+
+    if (pets === "javascript") {
+      javascriptScore += 1;
+    } else if (pets === "python") {
+      pythonScore += 1;
+    } else if (pets === "sharp") {
+      sharpScore += 1;
+    } else {
+      alert("You didn't answer you fuck!");
+    }
+
+    // Score Branch
+
+    if (javascriptScore > pythonScore && javascriptScore > sharpScore) {
+      $("#suggested-language").text("JavaScript");
+    } else if (pythonScore > javascriptScore && pythonScore > sharpScore) {
+      $("#suggested-language").text("Python");
+    } else if (sharpScore > javascriptScore && sharpScore > pythonScore) {
+      $("#suggested-language").text("C#");
+    } else {
+      $("#suggested-language").text("Hmm... could not process. Take again, you piece of shit.");
+    }
 
     $("#answers").show();
 
